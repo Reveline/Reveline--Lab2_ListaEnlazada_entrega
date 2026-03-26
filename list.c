ec7c5ed1e3228aed1072662787032724cc18586d
Your Name <you@example.com>
2026-03-26 17:18:36 +0000

    new try

--- list.c
+++ list.c
@@ -45,6 +45,7 @@ List * createList() {
 //   - La segunda función retorna el dato del nodo a continuación del current y actualiza el current para que apunte a ese nodo.
 
 void * firstList(List * list) {
+    assert(list->head != NULL);
     list->current = list->head;
     return list->current->data;
 }
